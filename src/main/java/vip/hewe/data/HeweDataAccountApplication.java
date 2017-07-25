74a831322f3d3c17644807e122e914c3feaad98f
heweifeng <heweifeng@autohome.com.cn>
2017-07-25 17:46:51 +0800

    修改为 dubbo

--- src/main/java/vip/hewe/data/HeweDataAccountApplication.java
+++ src/main/java/vip/hewe/data/HeweDataAccountApplication.java
@@ -1,53 +1,23 @@
 package vip.hewe.data;
 
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import vip.hewe.data.service.SignService;
+
+import java.io.IOException;
 
 @SpringBootApplication
-public class HeweDataAccountApplication implements CommandLineRunner {
+public class HeweDataAccountApplication {
 
     private static final Logger log = LoggerFactory.getLogger(HeweDataAccountApplication.class);
 
-    @Autowired
-    private SignService signService;
 
-    private static Server server;
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(HeweDataAccountApplication.class, args);
+        System.in.read();
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-        int port = 50051;
-
-        server = ServerBuilder.forPort(port).addService(signService).build().start();
-        log.info("server started, listen on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                if (server != null) {
-                    server.shutdown();
-                }
-                System.err.println("*** server shut down");
-            }
-        });
-
-        if (server != null) {
-            server.awaitTermination();
-        }
-
-    }
 }
 
 
